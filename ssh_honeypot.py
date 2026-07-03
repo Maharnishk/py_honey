47d0efa5e6e54d17581cfed157029c425b93ea63
Maharnishk <iammaharnish@gmail.com>
2026-07-04 00:41:21 +0530

    updated paramiko ssh server

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -2,6 +2,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import socket
+import paramiko
 #Constants
 logging_format=logging.Formatter('%(messsage)s')
 #loggers & loggings files
@@ -47,3 +48,37 @@ def emulated_shell(channel,client_ip):
         channel.send(response)
         channel.send(b'corporate-jumpbox2$ ')
         channel = b""
+
+# SSH server + sockets
+class server(paramiko.ServerInterface):
+    def __init__(self,client_ip,input_username=None,input_password=None):
+        self.client_ip = client_ip
+        self.input_username = input_username
+        self.input_password = input_password
+
+    def check_channel_request(self, kind, chanid:int)-> int :
+        if kind =='session':
+            return paramiko.OPEN_SUCCEEDED
+        
+    def get_allowed_auth(self):
+        return "password"
+    
+    def check_auth_password(self, username, password):
+
+        if self.input_username is not None and self.input_password is not None:
+            if username == 'username' and password == 'password':
+                return paramiko.AUTH_SUCCESSFULL
+            else:
+                return paramiko.AUTH_FAILED
+
+    def check_channel_shell_request(self, channel):
+        self.event.set()
+        return True
+    
+    def check_channel_pty_request(self, channel, term, width, height, pixelwidth, pixelheight, modes):
+        return True
+    
+    def check_channel_exec_request(self, channel, command):
+        command = str(command)
+        return True
+    
